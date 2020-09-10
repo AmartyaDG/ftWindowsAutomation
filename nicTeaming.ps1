883b838d74b5d17c2dcdae291a83fc059beef951
Amartya <amartya.dasgupta@stratus.com>
2020-09-10 19:52:40 +0530

    Update nicTeaming.ps1
    Updated comments and few minor syntax changes

--- nicTeaming.ps1
+++ nicTeaming.ps1
@@ -14,22 +14,13 @@ What the program does:
 
 Function createTeam
 {
-      <#
-      $interface = $nic_hash."$68_subnet[0]"
-      $x=$68_subnet[0]
-      Write-Host "nic_hash.x" $nic_hash."$x"
-      #>
-
-    #  sleep time in seconds
-    $SleepBetweenTeamChanges = 5  ## standard value = 5 sec
+    $SleepBetweenTeamChanges = 5  ##  sleep time in seconds
     Write-Host "Start creating teams..............."
 
     $ethernet_info = @(Get-NetIPAddress -AddressFamily IPv4) # Get all available Ethernet info
-    #$cmd > Get-NetIpAddress.txt
-    $ipAddress = @($ethernet_info.IPAddress) # array of all ip addresses  present in server
-    #Write-Host $ipAddress
-    $interfaceAlias = @($ethernet_info.InterfaceAlias) # array of all ethernets present in server
-    $nic_hash = @{} #create new empty hashtable with Key=Ip, Value = Ethernet
+    $ipAddress = @($ethernet_info.IPAddress)                 # array of all ip addresses  present in server
+    $interfaceAlias = @($ethernet_info.InterfaceAlias)       # array of all ethernets present in server
+    $nic_hash = @{}                                          #create new empty hashtable with Key=Ip, Value = Ethernet
         for ($i = 0; $i -lt $ipAddress.Count; $i++)
             {
                 $nic_hash[$ipAddress[$i]] = $interfaceAlias[$i] # new hashtable with key=IP, Value=Ethernet
@@ -37,16 +28,14 @@ Function createTeam
     $68_subnet = @($nic_hash.Keys | Select-String -Pattern '192.168.68') #filter out .68x subnet IPs.
     $61_subnet = @($nic_hash.Keys | Select-String -Pattern '192.168.61') #filter out .61x subnet IPs.
     $59_subnet = @($nic_hash.Keys | Select-String -Pattern '192.168.59') #filter out .59x subnet IPs.
-    if ( $68_subnet.Length -gt 1) # create nic-team if more than one .68 subnet IP is present
+    if ( $68_subnet.Length -gt 1)            # check more than one .68 subnet IP is available
     {
         $NIC1 = $68_subnet[0]
         $NIC2 = $68_subnet[1]
-        #write-host $nic_hash."$NIC2" ":" $nic_hash."$NIC1"
-        #write-host $NIC2 ":" $NIC1
-        New-NetLbfoTeam -Name "68-nic" -TeamMembers $nic_hash."$NIC1", $nic_hash."$NIC2" -Confirm:$false | Out-Null
+        New-NetLbfoTeam -Name "68-nic" -TeamMembers $nic_hash."$NIC1", $nic_hash."$NIC2" -Confirm:$false | Out-Null #Create Team
         sleep $SleepBetweenTeamChanges 
         if ($? -eq $false)   {
-               $msg = "ERROR: Team creation for $tn failed on try $teamCreatedTryCount ."
+               #$msg = "ERROR: Team creation for $tn failed on try $teamCreatedTryCount ."
                Write-Error -msgS E -msgText "Failed to create New-NetLbfoTeam"
                continue
             }<#else {
@@ -57,14 +46,14 @@ Function createTeam
 
      }
 
-     if ( $61_subnet.Length -gt 1) # create nic-team if more than one .61 subnet IP is present
+     if ( $61_subnet.Length -gt 1) # check more than one .68 subnet IP is available
     {
         $NIC1 = $61_subnet[0]
         $NIC2 = $61_subnet[1]
         New-NetLbfoTeam -Name "61-nic" -TeamMembers $nic_hash."$NIC1", $nic_hash."$NIC2" -Confirm:$false | Out-Null #Creates new team for .61 subnet
         sleep $SleepBetweenTeamChanges 
         if ($? -eq $false)   {
-               $msg = "ERROR: Team creation for $tn failed on try $teamCreatedTryCount ."
+               #$msg = "ERROR: Team creation for $tn failed on try $teamCreatedTryCount ."
                Write-Error -msgS E -msgText "Failed to create New-NetLbfoTeam"
                continue
             }<#else {
@@ -75,16 +64,14 @@ Function createTeam
 
      }
 
-     if ( $59_subnet.Length -gt 1) # create nic-team if more than one .59 subnet IP is present
+     if ( $59_subnet.Length -gt 1) # check more than one .68 subnet IP is available
     {
         $NIC1 = $59_subnet[0]
         $NIC2 = $59_subnet[1]
-        #write-host $nic_hash."$NIC2" ":" $nic_hash."$NIC1"
-        #write-host $NIC2 ":" $NIC1
-        New-NetLbfoTeam -Name "59-nic" -TeamMembers $nic_hash."$NIC1", $nic_hash."$NIC2" -Confirm:$false | Out-Null
+        New-NetLbfoTeam -Name "59-nic" -TeamMembers $nic_hash."$NIC1", $nic_hash."$NIC2" -Confirm:$false | Out-Null #Create Team
         sleep $SleepBetweenTeamChanges 
         if ($? -eq $false)   {
-               $msg = "ERROR: Team creation for $tn failed on try $teamCreatedTryCount ."
+               #$msg = "ERROR: Team creation for $tn failed on try $teamCreatedTryCount ."
                Write-Error -msgS E -msgText "Failed to create New-NetLbfoTeam"
                continue
             }<#else {
@@ -103,14 +90,8 @@ Function createTeam
 
 Function removeExistingTeam
 {
-      <#
-      $interface = $nic_hash."$68_subnet[0]"
-      $x=$68_subnet[0]
-      Write-Host "nic_hash.x" $nic_hash."$x"
-      #>
-
-    #  sleep time in seconds
-    $SleepBetweenTeamChanges = 5  ## standard value = 5 sec
+    
+    $SleepBetweenTeamChanges = 5  ##  sleep time in seconds
     $cc = Get-NetLbfoTeam
     Write-Verbose "Just got Teams.."
     $currentTeams = @(Get-NetLbfoTeam) #if any team already present, then need to remove
